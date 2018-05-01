bc8cfccd489caa257ef1dba97dbc6318bfe4a31a
filipo1299 <38488109+filipo1299@users.noreply.github.com>
2018-05-01 13:59:05 +0200

    Update EReservationException.php

--- EReservationException.php
+++ EReservationException.php
@@ -3,9 +3,11 @@
 class EReservationException extends LogicException {
     function __construct($roomNumber, Reservation $reservation)
     {
-        $this->message = "Room ". $roomNumber ." is already occupied for period from "
-            . $reservation->getStartDate()->format("d-m-y"). " to "
-            . $reservation->getEndDate()->format("d-m-y") . PHP_EOL
+        $this->message = " <b style ='color:#ff0000'>Room </b>". "<b style ='color:#ff0000'>$roomNumber</b>"
+            ."  <b style ='color:#ff0000'>is already occupied for period from </b>"
+            . "<b style ='color:#ff0000'>{$reservation->getStartDate()->format("d-m-y")}</b>"
+            . " <b style ='color:#ff0000'>to</b> "
+            . "<b style ='color:#ff0000'>{$reservation->getEndDate()->format("d-m-y") }</b>". PHP_EOL
             ."<br>";
     }
 }
